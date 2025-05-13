849715f0e1ab3a0a71ae0cd068b44efa90619f9b
Dino Muslic <dino.muslic51@gmail.com>
2025-05-13 16:06:07 +0200

    index.tsx automatically redirects to login page

--- app/index.tsx
+++ app/index.tsx
@@ -1,23 +1,21 @@
 import {
+  ActivityIndicator,
   StyleSheet,
-  Text,
-  TouchableOpacity,
   View,
 } from "react-native";
+import { useEffect } from "react";
 import { useRouter } from "expo-router";
 
 const Page = () => {
   const router = useRouter();
+
+  useEffect(() => {
+    router.replace("/login");
+  }, []);
+
   return (
     <View style={styles.container}>
-      <View style={styles.textContainer}>
-        <Text style={styles.h1}>Flower Shop</Text>
-        <Text style={styles.text}>Welcome to the Flower shop</Text>
-        <Text style={styles.text}>To proceed click the Button below</Text>
-        <TouchableOpacity onPress={() => router.push("/register")}>
-          <Text style={styles.register}>Register</Text>
-        </TouchableOpacity>
-      </View>
+      <ActivityIndicator size={"large"} color={"#00A0B6"}/>
     </View>
   );
 };
@@ -29,7 +27,6 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: "center",
     justifyContent: "center",
-    padding: 24,
   },
   h1: {
     fontSize: 32,
