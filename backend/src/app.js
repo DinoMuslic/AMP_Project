fe151a5811b3fda44f505361b881e6b1710bfc26
Dino Muslic <dino.muslic51@gmail.com>
2025-05-20 21:42:50 +0200

    Auth & User routes

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,9 +6,12 @@ const cors = require("cors");
 app.use(cors());
 app.use(express.json());
 
+const authRoutes = require("./routes/authRoutes");
 const flowerRoutes = require("./routes/flowerRoutes");
+const userRoutes = require("./routes/userRoutes");
 
+app.use("/api/auth", authRoutes);
 app.use("/api/flowers", flowerRoutes);
-
+app.use("/api/users", userRoutes);
 
 module.exports = app;
